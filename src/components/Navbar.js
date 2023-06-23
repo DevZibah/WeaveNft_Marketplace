51d2b9b0d528383ea8544a896030c493f85926ee
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-06-23 09:45:00 +0100

    saving progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRef } from "react";
-
+import "./navbar.css";
 import { useState } from "react";
 
 function Navbar() {
+  const navRef = useRef(null);
+
   const [togglerNav, setTogglerNav] = useState(false);
 
   const clickHandler = () => {
     setTogglerNav(!togglerNav);
   };
-
-  const navRef = useRef();
 
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav");
@@ -24,63 +24,69 @@ function Navbar() {
         src="./images/logo.png"
         alt="logo"
       />
-      <nav className="flex justify-between md:items-center">
-        <NavLink
-          className="nav-link text-white text-opacity-90 text-[20px] font-normal"
-          onClick={clickHandler}
-          to="/"
-        >
-          Home
-        </NavLink>
-        <NavLink
-          className="nav-link text-white text-opacity-90
+      <nav className=" md:items-center md:flex justify-between" ref={navRef}>
+        <div className="flex flex-col md:flex-row justify-between gap-2 md:gap-0">
+          <NavLink
+            className="nav-link text-white text-opacity-90 text-[20px] font-normal"
+            onClick={clickHandler}
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className="nav-link text-white text-opacity-90
                text-[20px] font-normal"
-          onClick={clickHandler}
-          to="/about"
-        >
-          About Us
-        </NavLink>
-        <NavLink
-          className="nav-link text-white text-opacity-90 
+            onClick={clickHandler}
+            to="/about"
+          >
+            About Us
+          </NavLink>
+          <NavLink
+            className="nav-link text-white text-opacity-90 
               text-[20px] font-normal"
-          onClick={clickHandler}
-          to="/contact"
-        >
-          Contact
-        </NavLink>
-        <NavLink
-          className="nav-link text-white text-opacity-90
+            onClick={clickHandler}
+            to="/contact"
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            className="nav-link text-white text-opacity-90
                text-[20px] font-normal"
-          onClick={clickHandler}
-          to="/about"
-        >
-          faq
-        </NavLink>
-        <NavLink
-          className="nav-link text-white text-opacity-90 
+            onClick={clickHandler}
+            to="/about"
+          >
+            faq
+          </NavLink>
+          <NavLink
+            className="nav-link text-white text-opacity-90 
               text-[20px] font-normal"
-          onClick={clickHandler}
-          to="/contact"
-        >
-          explore
-        </NavLink>
-        {/** connect wallet */}
-        <button
-          className="text-[12px] md:text-[20px] font-semibold 
+            onClick={clickHandler}
+            to="/contact"
+          >
+            explore
+          </NavLink>
+        </div>
+
+        <div>
+          {/** connect wallet */}
+
+          <button
+            className="text-[12px] md:text-[20px] font-semibold 
          text-white h-[50px]  w-[110px] md:w-[186px] px-[2px] 
-          py-[2px] md:py-[10px] md:px-[20px] hover:bg-pink-700 
+          py-[2px] md:py-[10px] md:px-[20px] 
           bg-gradient-to-br from-pink-700 to-violet-950 rounded-lg 
           justify-center items-center gap-2 flex"
-        >
-          Connect wallet
-        </button>
+          >
+            Connect wallet
+          </button>
+        </div>
         {/** close icon connection */}
         <button className="nav-btn nav-close-btn" onClick={showNavbar}>
           <FaTimes />
         </button>
       </nav>
       {/**show icon connection */}
-      <button className="nav-btn" onClick={showNavbar}>
+      <button className="nav-btn text-wh" onClick={showNavbar}>
         <FaBars />
       </button>
     </header>
