32bfe1f995e369a8f7bdc18c362b3176ce550423
Chisom Chukwuma <chukwumachisom18@gmail.com>
2023-07-03 04:37:35 -1100

    working on the nftdetails

---
+++ src/nft.js
@@ -0,0 +1,63 @@
+const data = [
+  {
+    id: 1,
+    image:
+      'https://res.cloudinary.com/sohmmie/image/upload/v1678217285/NFT1_vejmlu.avif',
+    heading: 'Ordit Pie',
+    text: 'lorem ipsum lorem ipsum',
+    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+    address: '1eRvx7rsh64RE74VGjn%DYR32cSD',
+    price: '95 ETH',
+  },
+  {
+    id: 2,
+    image:
+      'https://res.cloudinary.com/sohmmie/image/upload/v1678217285/NFT2_degfm2.jpg',
+    heading: 'Norb Cook',
+    text: 'lorem ipsum lorem ipsum',
+    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+    address: '8hTdsx7rsh64RE74VGjn%DYR32cSD',
+    price: '50 ETH',
+  },
+  {
+    id: 3,
+    image:
+      'https://res.cloudinary.com/sohmmie/image/upload/v1678217284/NFT3_gmodwz.jpg',
+    heading: 'Ice Dits',
+    text: 'lorem ipsum lorem ipsum',
+    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+    address: '00Fr5ex7rsh64RE74VGjn%DYR32cSD',
+    price: '67 ETH',
+  },
+  {
+    id: 4,
+    image:
+      'https://res.cloudinary.com/sohmmie/image/upload/v1678217285/NFT4_b8rxqh.png',
+    heading: 'Mochella Art',
+    text: 'lorem ipsum lorem ipsum',
+    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+    address: '2W1Af7rsh64RE74VGjn%DYR32cSD',
+    price: '37 ETH',
+  },
+  {
+    id: 5,
+    image:
+      'https://res.cloudinary.com/sohmmie/image/upload/v1678217285/NFT5_cvvhag.png',
+    heading: 'Smoke Hollow',
+    text: 'lorem ipsum lorem ipsum',
+    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+    address: '5Rf4rsh64RE74VGjn%DYR32cSD',
+    price: '17 ETH',
+  },
+  {
+    id: 6,
+    image:
+      'https://res.cloudinary.com/sohmmie/image/upload/v1678217285/NFT6_voaac9.webp',
+    heading: 'Slick Ghost',
+    text: 'lorem ipsum lorem ipsum',
+    description: 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum',
+    address: '9Ix7rsh64RE76F4Dv^jn%DYR32cSD',
+    price: '97 ETH',
+  },
+]
+export default data
